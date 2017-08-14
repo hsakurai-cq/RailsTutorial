657d2493f9e112de289460762308e83e4db54c89
hsakurai-cq <h.sakurai@caraquri.com>
2017-08-14 11:01:05 +0900

    Finished Chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #save data
+      logger.info("Success, creating user")
+      redirect_to @user
     else
       render "new"
     end
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password,:password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
 
 end
